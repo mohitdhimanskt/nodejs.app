c764c04a068f28ebc753df3c005c897859483e6f
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-04 11:46:17 +0530

    Add t0 senmail

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.render('contact');
 });
+
 app.post('/send', (req, res) => {
   const output = `
     <p>You have a new contact request</p>
@@ -47,12 +48,26 @@ app.post('/send', (req, res) => {
       rejectUnauthorized:false
     }
   });
+
   // setup email data with unicode symbols
   let mailOptions = {
-    from: '"Nodemailer Contact" <your@email.com>', // sender address
-    to: 'RECEIVEREMAILS', // list of receivers
-    subject: 'Node Contact Request', // Subject line
-    text: 'Hello world?', // plain text body
-    html: output // html body
-};
+      from: '"Nodemailer Contact" <your@email.com>', // sender address
+      to: 'RECEIVEREMAILS', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
 
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+  });
+  });
+
+app.listen(3000, () => console.log('Server started...'));
