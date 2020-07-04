53b85ca04b0ac3c1909145c890333efb1231887c
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-04 11:42:16 +0530

    Add to transporter

--- app.js
+++ app.js
@@ -33,3 +33,17 @@ app.post('/send', (req, res) => {
     <h3>Message</h3>
     <p>${req.body.message}</p>
   `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'mail.YOURDOMAIN.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'YOUREMAIL', // generated ethereal user
+        pass: 'YOURPASSWORD'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
